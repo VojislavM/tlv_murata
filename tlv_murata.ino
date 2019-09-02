baed01137497ce82a5b5c6f0a589ca802674ddd5
musti <musti@wlan-si.net>
2019-09-02 17:09:00 +0200

    Implemented logic with serial interrupt vector

--- tlv_murata.ino
+++ tlv_murata.ino
@@ -1,9 +1,8 @@
 #include "frame.h"
 #include "message.h"
+#include "STM32L0.h"
 
-int32_t lon = 42;
-int32_t lat = 16;
-int togl_led = 0;
+#define HORIZON_SERIAL Serial1
 
 parser_t parser;
 tlv_gps_location_t loc = {
@@ -15,75 +14,63 @@ tlv_gps_location_t loc = {
             .v_acc = 1,
         };
 
-static void my_incoming_message_handler(const message_t *message);
+boolean horizon_gps_flag = false;
+static void horizon_message_callback(const message_t *message);
+void horizon_serial_callback();
 
 void setup() {
     Serial.begin(115200);
+    Serial.println("boot()");
     // initialize digital pin LED_BUILTIN as an output.
     pinMode(LED_BUILTIN, OUTPUT);
 
+    HORIZON_SERIAL.begin(115200);
+    HORIZON_SERIAL.setWakeup(1);
+    HORIZON_SERIAL.onReceive(horizon_serial_callback);
     frame_parser_init(&parser);
-    parser.handler = my_incoming_message_handler;
+    parser.handler = horizon_message_callback;
 }
 
 // the loop function runs over and over again forever
 void loop() {
-    // digitalWrite(LED_BUILTIN, togl_led);   // turn the LED on (HIGH is the voltage level)
-    // delay(1000);                       // wait for a second 
-    // togl_led = !togl_led;
-    
-    // message_t msg;
-    // message_init(&msg);
-    // tlv_gps_location_t loc = {
-    //     .itow = 123,
-    //     .lon = lon,
-    //     .lat = lat,
-    //     .h_msl = 296,
-    //     .h_acc = 1,
-    //     .v_acc = 1,
-    // };
-    // message_tlv_add_gps_location(&msg, &loc);
-
-    // // Frame message.
-    // uint8_t frame[64];
-    // ssize_t frame_size = frame_message(frame, sizeof(frame), &msg);
-    // if (frame_size < 0) {
-    //     Serial.println("Failed to frame message!");
-    //     return;
-    // }
-
-    // //Serial.println("Sending framed message:\n");
-    // //Serial.write(frame, frame_size);
-    //  for (size_t j = 0; j < frame_size; j++) {
-    //      Serial.print(frame[j], HEX);
-    //  }
-    // Serial.println();
-
-    // // Increment lat/long.
-    // lat = (lat + 1) % 90;
-    // lon = (lon + 1) % 90;              
-}
-
-void serialEvent() {
-  while (Serial.available()) {
-    // get the new byte;
-    char inChar = (char)Serial.read();
-    uint8_t uiChar = (uint8_t)inChar;
-    // When you receive some bytes from the serial port you call.
-    frame_parser_push_buffer(&parser, &uiChar, 1);
-  }
-}
-
-// callback will be called when a message is received.
-static void my_incoming_message_handler(const message_t *message) {
-    message_print(message);
-    if(message_tlv_get_gps_location(message, &loc) == MESSAGE_SUCCESS){
+    STM32L0.stop();
+    if(horizon_gps_flag){
+        horizon_gps_flag=false;
         Serial.print("ok lon: ");
         Serial.print((int)loc.lon);
         Serial.print(", ");
         Serial.print("lat: ");
-        Serial.println((int)loc.lat);
-    }else{
-        Serial.println("not ok");
+        Serial.print((int)loc.lat);
+        Serial.print(", ");
+        Serial.print("itow: ");
+        Serial.println((int)loc.itow);
+    }          
+}
+
+/**
+ * @brief callback on serial message received from the horizon device
+ * 
+ */
+void horizon_serial_callback() {
+    uint8_t rx_data[FRAME_MAX_LENGTH];
+    int rx_count;
+    do{
+        rx_count = Serial1.read(&rx_data[0], sizeof(rx_data));
+        if (rx_count > 0){         
+            frame_parser_push_buffer(&parser, &rx_data[0], rx_count);
+        }
+    }
+    while (rx_count > 0);
+}
+
+/**
+ * @brief callback from the message decoder on successfulr eception of a TLV
+ * 
+ * @param message 
+ */
+static void horizon_message_callback(const message_t *message) {
+    if(message_tlv_get_gps_location(message, &loc) == MESSAGE_SUCCESS){
+        horizon_gps_flag=true;
+        STM32L0.wakeup();
     }
 }
